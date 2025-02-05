54611b558e82eeee3339e0fb717fd72f4158cefd
Ivan Aleksandrovskii <ivan.vered@gmail.com>
2025-02-05 10:22:47 +0700

    spinner fix

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -6,7 +6,7 @@ import React from "react";
 const LoadingSpinner = () => {
     return (
         <div className="flex justify-center items-center mt-8">
-            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900 mt-4"></div>
+            <div className="animate-spin rounded-full h-16 w-16 border-b-4 border-blue-500"></div>
         </div>
     );
 };
