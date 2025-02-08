4dd472d814cae17f7026d73c5659fa243fdba851
Ivan Aleksandrovskii <ivan.vered@gmail.com>
2025-02-09 01:42:52 +0700

    temp env fix

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,4 +1,4 @@
 // src/api/constants.js
 
-export const BOT_USERNAME = import.meta.env.VITE_APP_BOT_USERNAME || "MyTelegramBot";
-export const BASE_URL = import.meta.env.VITE_APP_API_BASE_URL || 'https://example.com';
+export const BOT_USERNAME = import.meta.env.VITE_APP_BOT_USERNAME || "tap_quiz_bot";
+export const BASE_URL = import.meta.env.VITE_APP_API_BASE_URL || 'https://test.sendmoneyto.ru';
